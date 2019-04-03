8fa1aed8a71a9addac94cb239aaacce34a2e6ef8
Konstantin Tornovskiy <coolship@yandex.ru>
2019-04-03 19:21:51 +0300

    Create some base HTML page. In settings.py STATIC_ROOT added. Start authapp. Write urls.py in authapp.

---
+++ authapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+import authapp.views as authapp
+
+# Required for Django 2.0 an above
+app_name = 'authapp'
+
+urlpatterns = [
+    re_path('^login/$', authapp.login, name='login'),
+    re_path('^logout/$', authapp.logout, name='logout'),
+]
